e20d2170ca6271c75f71f49ff2d36eeca11222ce
Dev Ranasinghe <999sasmith@gmail.com>
2025-03-18 14:26:04 +0530

    navbar structure and pages added

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,19 @@
-import React from 'react'
+import {Route, BrowserRouter as Router, Routes} from 'react-router-dom'
+import Navbar from './components/Navbar'
 
-function App() {
+const App = () => {
   return (
-    <div>
-      <h1 className='text-4xl text-center underline text-blue-500'>
-        Hello
-      </h1>
-    </div>
+    <main className='bg-slate-300/20'>
+      <Router>
+        <Navbar/>
+        <Routes>
+          <Route path='/' element={'Home'} />
+          <Route path='/about' element={'About'} />
+          <Route path='/projects' element={'Projects'} />
+          <Route path='/contact' element={'Contact'} />
+        </Routes>
+      </Router>
+    </main>
   )
 }
 
