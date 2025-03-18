917008c81c585b7dc7ec45e1551008562d25ffa0
Dev Ranasinghe <999sasmith@gmail.com>
2025-03-18 15:54:45 +0530

    sky 3D element added

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,6 +3,7 @@ import { Canvas } from '@react-three/fiber'
 import { Suspense } from 'react'
 import Loader from '../components/Loader'
 import Island from '../models/Island'
+import Sky from '../models/sky'
 
 function Home() {
 
@@ -28,13 +29,14 @@ function Home() {
             Popup
         </div> */}
 
-        <Canvas className='w-full h-full bg-transparent' camera={{near: 0.1, far: 1000}}>
+        <Canvas 
+            className='w-full h-full bg-transparent' 
+            camera={{near: 0.1, far: 1000}}>
             <Suspense fallback={<Loader/>}>
-                <directionalLight/>
-                <ambientLight/>
-                <pointLight/>
-                <spotLight/>
-                <hemisphereLight/>
+                <directionalLight position={[1,1,1]} intensity={2} />
+                <ambientLight intensity={0.5} />
+                <hemisphereLight skyColor='#b1e1ff' groundColor="#000000" intensity={1} />
+                <Sky />
                 <Island 
                     position={islandPosition}
                     scale={islandScale} 
