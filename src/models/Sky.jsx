2a65cd00646140775c47e14c47213212ada1a168
Dev Ranasinghe <999sasmith@gmail.com>
2025-03-24 09:53:28 +0530

    sky rotation and animation done

--- src/models/Sky.jsx
+++ src/models/Sky.jsx
@@ -1,15 +1,24 @@
-import { useGLTF } from '@react-three/drei'
-import React from 'react'
+import { useRef } from "react";
+import { useGLTF } from "@react-three/drei";
+import { useFrame } from "@react-three/fiber";
 
-import skyScene from '../assets/3d/sky.glb'
+import skyScene from "../assets/3d/sky.glb";
 
-const Sky = () => {
-    const sky = useGLTF(skyScene)
-    return (
-        <mesh>
-            <primitive object={sky.scene} />
-        </mesh>
-    )
+export function Sky({ isRotating }) {
+  const sky = useGLTF(skyScene);
+  const skyRef = useRef();
+
+  useFrame((_, delta) => {
+    if (isRotating) {
+      skyRef.current.rotation.y += 0.25 * delta; // Adjust the rotation speed as needed
+    }
+  });
+
+  return (
+    <mesh ref={skyRef}>
+      // use the primitive element when you want to directly embed a complex 3D
+      model or scene
+      <primitive object={sky.scene} />
+    </mesh>
+  );
 }
-
-export default Sky
